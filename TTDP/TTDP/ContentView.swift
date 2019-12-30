e5f4fa42d2bc8e13c84414a6234c6dda22ea0b73
ttdp <tiantong.dev@gmail.com>
2019-12-30 14:45:22 +0800

    Add TTDP

---
+++ TTDP/TTDP/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TTDP
+//
+//  Created by Tian Tong on 2019/12/30.
+//  Copyright © 2019 TTDP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
