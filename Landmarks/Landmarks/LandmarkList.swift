d1a9712a9585e7876579a0a7fb7c8f3ab693cf5d
ttdp <tiantong.dev@gmail.com>
2019-12-31 15:51:02 +0800

    Add CategoryHome

--- Landmarks/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -12,32 +12,31 @@ struct LandmarkList: View {
     @EnvironmentObject var userData: UserData
     
     var body: some View {
-        NavigationView {
-            List {
-                Toggle(isOn: $userData.showFavoritesOnly) {
-                    Text("Favorites only")
-                }
-                
-                ForEach(userData.landmarks) { landmark in
-                    if !self.userData.showFavoritesOnly || landmark.isFavorite {
-                        NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                            LandmarkRow(landmark: landmark)
-                        }
+        List {
+            Toggle(isOn: $userData.showFavoritesOnly) {
+                Text("Favorites only")
+            }
+            
+            ForEach(userData.landmarks) { landmark in
+                if !self.userData.showFavoritesOnly || landmark.isFavorite {
+                    NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                        LandmarkRow(landmark: landmark)
                     }
                 }
-                
             }
-        .navigationBarTitle(Text("Landmarks"))
         }
+        .navigationBarTitle(Text("Landmarks"))
     }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(["iPhone 11 Pro"], id: \.self) { deviceName in
-            LandmarkList()
-            .environmentObject(UserData())
-                .previewDevice(PreviewDevice(rawValue: deviceName))
+            NavigationView {
+                LandmarkList()
+                    .environmentObject(UserData())
+                    .previewDevice(PreviewDevice(rawValue: deviceName))
+            }
         }
     }
 }
