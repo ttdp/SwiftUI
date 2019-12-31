788e47c54839ef5641b1ea7f2b9d380cfd5040e1
ttdp <tiantong.dev@gmail.com>
2019-12-31 13:51:23 +0800

    Add Badge

---
+++ Landmarks/Landmarks/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Tian Tong on 2019/12/31.
+//  Copyright © 2019 TTDP. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
